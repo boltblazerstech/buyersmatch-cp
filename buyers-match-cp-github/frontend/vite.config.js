f1edb4cb1a121c5842a523b6b9b8728dadf2bbd1
CandidateMaster2002 <kickmegreen@gmail.com>
2026-07-02 19:39:17 +0530

    vite rror fixed

--- buyers-match-cp-github/frontend/vite.config.js
+++ buyers-match-cp-github/frontend/vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
+  server: {
+    host: true,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
